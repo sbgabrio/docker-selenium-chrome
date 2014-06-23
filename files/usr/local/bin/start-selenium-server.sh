d1022be31771d418d7890459703ea77e1f184665
Jeremy Seago <jseago@bellevuerx.com>
2014-06-23 14:09:10 -0500

    updates

--- files/usr/local/bin/start-selenium-server.sh
+++ files/usr/local/bin/start-selenium-server.sh
@@ -4,5 +4,5 @@ export DISPLAY=:10
 # /etc/init.d/xvfb start
 Xvfb :10 -screen 0 1366x768x24 -ac &
 echo "Starting Google Chrome ..."
-sudo google-chrome --no-sandbox --remote-debugging-port=9222 &
+google-chrome --no-sandbox --remote-debugging-port=9222 &
 java -Dwebdriver.chrome.driver=/usr/local/bin/chromedriver -jar /usr/local/bin/selenium-server-standalone-2.42.2.jar
