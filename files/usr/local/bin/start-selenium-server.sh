c219b11b345cb4f443b2a3dc12dcf2a607587762
Joseph McElroy <phoey1@gmail.com>
2015-09-09 14:13:03 +0100

    Update start-selenium-server.sh

--- files/usr/local/bin/start-selenium-server.sh
+++ files/usr/local/bin/start-selenium-server.sh
@@ -5,4 +5,4 @@ export DISPLAY=:10
 Xvfb :10 -screen 0 1400x1200x24 -ac &
 echo "Starting Google Chrome ..."
 google-chrome --no-sandbox --remote-debugging-port=9222 &
-java -Dwebdriver.chrome.driver=/usr/local/bin/chromedriver -jar /usr/local/bin/selenium-server-standalone-2.42.2.jar
+java -Dwebdriver.chrome.driver=/usr/local/bin/chromedriver -jar /usr/local/bin/selenium-server-standalone-2.45.0.jar
