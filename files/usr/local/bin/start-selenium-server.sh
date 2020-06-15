af8cb1e43c1238042d86b66b9564022ead4d941c
Steve <stevegabrio@gmail.com>
2020-06-14 22:16:29 -0700

    Changed to executing jar
    jar contains the chromedriver startup

--- files/usr/local/bin/start-selenium-server.sh
+++ files/usr/local/bin/start-selenium-server.sh
@@ -3,6 +3,5 @@
 export DISPLAY=:10
 # /etc/init.d/xvfb start
 Xvfb :10 -screen 0 1400x1200x24 -ac &
-echo "Starting Google Chrome ..."
-google-chrome --no-sandbox --remote-debugging-port=9222 &
-java -Dwebdriver.chrome.driver=/usr/local/bin/chromedriver -jar /usr/local/bin/selenium-server-standalone-2.45.0.jar
+echo "Launching Test Jar ..."
+java -jar /usr/local/bin/gs2020.jar
